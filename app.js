3667ec0b6bc5c4575075bedbd2f8550739a7f828
Nifteliyev <aminnifteliyev45@gmail.com>
2025-01-28 22:57:31 +0400

    JStask.2

---
+++ app.js
@@ -0,0 +1,17 @@
+let fullName = "Amin Nifteliyev"
+console.log( fullName.split(" ")[1],fullName.split(" ")[0])
+
+
+
+
+
+
+console.log(fullName.split(" ")[0][0],fullName.split(" ")[1][0])
+
+
+
+
+
+
+
+console.log(fullName.length - 1)
